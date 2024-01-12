66f73a201bee8eadc56ef06f24cd3b18758d8139
Marcus Genzel <85606416+marcusgenzel@users.noreply.github.com>
2024-01-12 23:45:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 # app.py
+import altair as alt
+import numpy as np
 import pandas as pd
 import streamlit as st
 
-
-streamlit hello
 
 def main():
     st.title("My Streamlit App")
@@ -46,3 +46,41 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+st.header("st.button")
+
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+
+
+st.header("st.write")
+
+# Example 1
+
+st.write("Hello, *World!* :sunglasses:")
+
+# Example 2
+
+st.write(1234)
+
+# Example 3
+
+df = pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
+st.write(df)
+
+# Example 4
+
+st.write("Below is a DataFrame:", df, "Above is a dataframe.")
+
+# Example 5
+
+df2 = pd.DataFrame(np.random.randn(200, 3), columns=["a", "b", "c"])
+c = (
+    alt.Chart(df2)
+    .mark_circle()
+    .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
+)
+st.write(c)
