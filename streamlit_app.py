b293f09012008ca52cd872b8b06f2fb6459d4923
Marcus Genzel <85606416+marcusgenzel@users.noreply.github.com>
2024-01-12 23:57:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 # app.py
+from datetime import datetime, time
+
 import altair as alt
 import numpy as np
 import pandas as pd
@@ -84,3 +86,98 @@ c = (
     .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
 )
 st.write(c)
+
+
+st.subheader("This is a subheader with a divider", divider="rainbow")
+st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
+
+
+st.caption("This is a string that explains something above.")
+st.caption("A caption with _italics_ :blue[colors] and emojis :sunglasses:")
+
+
+st.text("This is some text.")
+
+st.latex(
+    r"""
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    """
+)
+
+code = """def hello():
+    print("Hello, Streamlit!")"""
+st.code(code, language="python")
+
+
+st.header("st.slider")
+
+# Example 1
+
+st.subheader("Slider")
+
+age = st.slider("How old are you?", 0, 130, 25)
+st.write("I'm ", age, "years old")
+
+# Example 2
+
+st.subheader("Range slider")
+
+values = st.slider("Select a range of values", 0.0, 100.0, (25.0, 75.0))
+st.write("Values:", values)
+
+# Example 3
+
+st.subheader("Range time slider")
+
+appointment = st.slider(
+    "Schedule your appointment:", value=(time(11, 30), time(12, 45))
+)
+st.write("You're scheduled for:", appointment)
+
+# Example 4
+
+st.subheader("Datetime slider")
+
+start_time = st.slider(
+    "When do you start?", value=datetime(2020, 1, 1, 9, 30), format="MM/DD/YY - hh:mm"
+)
+st.write("Start time:", start_time)
+st.header("Line chart")
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.line_chart(chart_data)
+
+from vega_datasets import data
+
+source = data.cars()
+
+chart = (
+    alt.Chart(source)
+    .mark_circle()
+    .encode(
+        x="Horsepower",
+        y="Miles_per_Gallon",
+        color="Origin",
+    )
+    .interactive()
+)
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Altair native theme"])
+
+with tab1:
+    # Use the Streamlit theme.
+    # This is the default. So you can also omit the theme argument.
+    st.altair_chart(chart, theme="streamlit", use_container_width=True)
+with tab2:
+    # Use the native Altair theme.
+    st.altair_chart(chart, theme=None, use_container_width=True)
+
+
+st.header("st.selectbox")
+
+option = st.selectbox("What is your favorite color?", ("Blue", "Red", "Green"))
+
+st.write("Your favorite color is ", option)
