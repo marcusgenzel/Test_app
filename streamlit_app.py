3f1990137773c1613427cfb3a1b0473f76dfb67c
Marcus Genzel <85606416+marcusgenzel@users.noreply.github.com>
2024-01-13 00:12:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -182,3 +182,21 @@ st.header("st.selectbox")
 option = st.selectbox("What is your favorite color?", ("Blue", "Red", "Green"))
 
 st.write("Your favorite color is ", option)
+
+
+st.header("st.checkbox")
+
+st.write("What would you like to order?")
+
+icecream = st.checkbox("Ice cream")
+coffee = st.checkbox("Coffee")
+cola = st.checkbox("Cola")
+
+if icecream:
+    st.write("Great! Here's some more 🍦")
+
+if coffee:
+    st.write("Okay, here's some coffee ☕")
+
+if cola:
+    st.write("Here you go 🥤")
