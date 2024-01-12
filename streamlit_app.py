cea5a46b811ce0b2e956903d0ae4a2bdce2b0157
Marcus Genzel <85606416+marcusgenzel@users.noreply.github.com>
2024-01-13 00:04:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -150,9 +150,8 @@ chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
 st.line_chart(chart_data)
 
-from vega_datasets import data
 
-source = data.cars()
+source = chart_data
 
 chart = (
     alt.Chart(source)
