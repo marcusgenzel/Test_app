bf8d24c500b70ae7346e3e885d6988860f0e8644
Marcus Genzel <85606416+marcusgenzel@users.noreply.github.com>
2024-01-13 10:32:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -209,3 +209,14 @@ df = pd.read_csv(
 
 pr = df.profile_report()
 st_profile_report(pr)
+
+
+st.header("st.latex")
+
+st.latex(
+    r"""
+     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)
+     """
+)
