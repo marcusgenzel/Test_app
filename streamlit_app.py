30fbcc0c4f4d27f3491b8d376e527d0e41c0fc56
Marcus Genzel <85606416+marcusgenzel@users.noreply.github.com>
2024-01-13 21:45:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Streamlit Option Menu",
     page_icon="🧊",
-    layout="wide",
+    # layout="wide",
     initial_sidebar_state="expanded",
 )
 
