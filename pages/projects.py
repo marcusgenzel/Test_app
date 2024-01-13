cadfc66f7dc0299dad161fbc02e435610cf6963e
Marcus Genzel <85606416+marcusgenzel@users.noreply.github.com>
2024-01-13 23:07:03 +0100

    Update projects.py

--- pages/projects.py
+++ pages/projects.py
@@ -1,7 +1,9 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+
 
 st.title("Projects")
-# st.write("You have entered", st.session_state["my_input"])
+st.write("You have entered", st.session_state["my_input"])
 
 
 with st.sidebar:
