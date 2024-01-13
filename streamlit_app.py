1bcb1f0d544936c6a22389a2015057874ea3bf51
Marcus Genzel <85606416+marcusgenzel@users.noreply.github.com>
2024-01-13 21:40:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -474,8 +474,6 @@ page_names_to_funcs = {
 demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys())
 page_names_to_funcs[demo_name]()
 
-import streamlit as st
-from streamlit_option_menu import option_menu
 
 st.set_page_config(
     page_title="Streamlit Option Menu",
